8b6ee0d1d8e83401ec6f5e3955bc734228598e2a
dan1mk <60133884+dan1mk@users.noreply.github.com>
2021-11-04 03:51:23 +0100

    update
    For Against texts

--- DrinkApp/Assets/Scripts/SetQuestion.cs
+++ DrinkApp/Assets/Scripts/SetQuestion.cs
@@ -16,6 +16,9 @@ public class SetQuestion : MonoBehaviour
         0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27 ,28, 29
     };
 
+    public GameObject For;
+    public GameObject Against;
+
     int random = 0;
     int temp = 0;
     // Start is called before the first frame update
@@ -38,6 +41,26 @@ public class SetQuestion : MonoBehaviour
         //{
         //    Debug.Log(usedRands[i]);
         //}
+    }
+
+    private void Update()
+    {
+        for(int i = 0; i < questions.Length; i++)
+        {
+            if(questions[i].activeInHierarchy)
+            {
+                if(i <= 12)
+                {
+                    For.SetActive(true);
+                    Against.SetActive(true);
+                }
+                else
+                {
+                    For.SetActive(false);
+                    Against.SetActive(false);
+                }
+            }
+        }
     }
 
 }
